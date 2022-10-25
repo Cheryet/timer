3abeb0ae2b7ed892ca754b8972823d22806de893
Cheryet <C.heryet@outlook.com>
2022-10-25 14:05:34 -0600

    eslinted timer1.js

--- timer1.js
+++ timer1.js
@@ -1,15 +1,15 @@
- //Beep sound
+//Beep sound
 
-let times = process.argv.slice(2)
+let times = process.argv.slice(2);
 
 const timer = () => {
-  for (let time of times){
-    if (time > 0){
+  for (let time of times) {
+    if (time > 0) {
       setTimeout(() => {
         process.stdout.write('\x07');
       }, time * 1000);
-    };
-  };
+    }
+  }
 };
 
 timer();
